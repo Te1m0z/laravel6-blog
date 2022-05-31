5a7138ad70574123653bec3e270e1fdb5b7de94f
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-05-31 23:05:41 +0400

    вывод index view Категорий в Админке

---
+++ resources/views/blog/admin/category/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+                <a class="btn btn-primary" href="{{ route('blog.admin.categories.create') }}">Добавить</a>
+            </nav>
+            <div class="card">
+                <table>
+                    <thead>
+                        <th>#</th>
+                        <th>Категория</th>
+                        <th>Родитель</th>
+                    </thead>
+                    <tbody>
+                        @foreach($paginator as $item)
+                            @php /** @var \App\Models\BlogCategory $item */ @endphp
+                            <tr>
+                                <td>{{ $item->id }}</td>
+                                <td>
+                                    <a href="{{ route('blog.admin.categories.edit', $item->id) }}">
+                                        {{ $item->title }}
+                                    </a>
+                                </td>
+                                <td @if(in_array($item->parent_id, [0, 1])) style="color: red;" @endif>
+                                    {{ $item->parent_id }}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
